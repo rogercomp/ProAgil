fbc2b1255fc31d78cc573ea118b7e8cf46ea233f
ROGERIO SIQUEIRA DE MIRANDA <rogerio.miranda@policard.com.br>
2019-09-04 09:03:54 -0300

    03 Angular

--- ProAgil-App/src/app/eventos/eventos.component.ts
+++ ProAgil-App/src/app/eventos/eventos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-eventos',
@@ -7,27 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  eventos : any = [
-    {
-      EventoId: 1,
-      Tema: 'Angular',
-      Local: 'Belo Horizonte'
-    },
-    {
-      EventoId: 2,
-      Tema: '.NET Core',
-      Local: 'Uberlândia'
-    },
-    {
-      EventoId: 3,
-      Tema: 'Angular .NET Core',
-      Local: 'Rio de Janeiro'
-    }
-  ];
-
-  constructor() { }
+  eventos : any;
+  
+  
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.getEventos();
   }
 
+  getEventos() {
+     this.http.get("http://localhost:5000/api/values").subscribe(response=> {
+      this.eventos = response;
+    }, error => {
+      console.log(error);
+    }
+    );
+  }
 }
